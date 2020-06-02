326cb97d56a6f14cd9b0daaa6d9e2f17efc55d1e
amatic <amatic@nanocosmos.de>
2020-06-02 10:36:18 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9884ba3af4864e3869a";
+/******/ 	var hotCurrentHash = "e8de4f25dcf55f984f29";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
